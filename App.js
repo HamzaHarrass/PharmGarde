0a89d133e7f100eb1ced36cb1ae927d2b42be596
HamzaHarrass <hamzaharrass05@gmail.com>
2024-02-12 16:07:03 +0100

    simple changement (color background/text/bouton) ajouter logo d'application qui afficher le debut de run l'application

--- App.js
+++ App.js
@@ -1,23 +1,30 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { StyleSheet, Text, View,Button, Image, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 import SignUpScreen from './SignUpScreen';
 import SignInScreen from './SignInScreen';
 import PharmacyScreen from './PharmacyScreen';
+
+// CustomButton pour contrôler le style du texte
+const CustomButton = ({ title, onPress }) => (
+  <TouchableOpacity onPress={onPress} style={styles.button}>
+    <Text style={styles.buttonText}>{title}</Text>
+  </TouchableOpacity>
+);
 
 // Écran d'accueil de l'application
 function HomeScreen({ navigation }) {
   return (
     <View style={styles.container}>
-    <Text>Welcome To PharmGarde</Text>
+      <Text style={styles.welcomeText}>Welcome To PharmGarde</Text>
       <Image source={require('./assets/991ce665cc7d0ee9b05e6881e5dab431.png')} style={styles.menuIcon} />
-    <View style={styles.Button}>
-    <Button  title="Sign In" onPress={() => navigation.navigate('SignIn')} />
-    <Button  title="Sign Up" onPress={() => navigation.navigate('SignUp')} />
-    <Button  title="Pharmacy" onPress={() => navigation.navigate('Pharmacy')} />
+      <View style={styles.Button}>
+        <CustomButton title="Sign In" onPress={() => navigation.navigate('SignIn')} />
+        <CustomButton title="Sign Up" onPress={() => navigation.navigate('SignUp')} />
+        <CustomButton title="Pharmacy" onPress={() => navigation.navigate('Pharmacy')} />
+      </View>
     </View>
-  </View>
   );
 }
 
@@ -35,30 +42,46 @@ const Stack = createStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-    <Stack.Navigator>
-      <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'PharmGarde', headerTitleAlign: 'center' }} />
-      <Stack.Screen name="SignIn" component={SignInScreen} options={{ title: 'Sign In', headerTitleAlign: 'center' }} />
-      <Stack.Screen name="SignUp" component={SignUpScreen} options={{ title: 'Sign Up', headerTitleAlign: 'center' }} />
-      <Stack.Screen name="Pharmacy" component={PharmacyScreen} options={{ title: 'Pharmacy', headerTitleAlign: 'center' }} />
-    </Stack.Navigator>
-  </NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'PharmGarde', headerTitleAlign: 'center' }} />
+        <Stack.Screen name="SignIn" component={SignInScreen} options={{ title: 'Sign In', headerTitleAlign: 'center' }} />
+        <Stack.Screen name="SignUp" component={SignUpScreen} options={{ title: 'Sign Up', headerTitleAlign: 'center' }} />
+        <Stack.Screen name="Pharmacy" component={PharmacyScreen} options={{ title: 'Pharmacy', headerTitleAlign: 'center' }} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#6FCF97', 
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  welcomeText: {
+    color: '#FFFFFF',
+    fontSize: 24,
+    marginBottom: 20,
   },
   menuIcon: {
     width: 200,
     height: 200,
     margin: 10,
   },
-  Button:{
-    display:'flex',
-    gap:5,
-  }
+  Button: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: '80%',
+  },
+  button: {
+    backgroundColor: '#FFFFFF',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: '#000000', // Couleur du texte noir
+    fontSize: 16,
+  },
 });
