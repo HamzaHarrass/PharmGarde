40105a81527ed14929ce45195d1bb49a3283b7ee
HamzaHarrass <hamzaharrass05@gmail.com>
2024-02-19 03:32:31 +0100

    changer style de screen sing Up

--- screen/SignUpScreen.js
+++ screen/SignUpScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, TextInput, Button, Image, StyleSheet, KeyboardAvoidingView } from 'react-native';
+import { View, Text, TextInput, Button, Image, ImageBackground, StyleSheet, KeyboardAvoidingView } from 'react-native';
 import { Formik } from 'formik';
 import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
 import { auth } from '../firebase';
@@ -22,78 +22,89 @@ export default function SignUpScreen({ navigation }) {
   };
 
   return (
-    <KeyboardAvoidingView style={styles.container}>
-      <Image source={Logo} style={styles.logo} />
+    <ImageBackground source={require('../assets/v870-tang-32.jpg')} style={styles.background}>
+      <KeyboardAvoidingView style={styles.container} behavior="padding">
+        <View style={styles.inputContainer}>
+          <Image source={Logo} style={styles.logo} />
+          
+          <Formik
+            initialValues={{ username: '', email: '', password: '' }}
+            onSubmit={(values) => handleSignUp(values)}
+            validate={(values) => {
+              const errors = {};
+              if (!values.username) {
+                errors.username = 'Le nom d\'utilisateur est requis';
+              }
+              if (!values.email) {
+                errors.email = 'L\'adresse email est requise';
+              } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(values.email)) {
+                errors.email = 'Adresse email invalide';
+              }
+              if (!values.password) {
+                errors.password = 'Le mot de passe est requis';
+              } else if (values.password.length < 6) {
+                errors.password = 'Le mot de passe doit contenir au moins 6 caractères';
+              }
+              return errors;
+            }}
+          >
+            {({ handleChange, handleBlur, handleSubmit, values, errors }) => (
+              <View>
+                <TextInput
+                  style={styles.input}
+                  placeholder="Username"
+                  onChangeText={handleChange('username')}
+                  onBlur={handleBlur('username')}
+                  value={values.username}
+                />
+                <Text style={styles.errorText}>{errors.username}</Text>
 
-      <Formik
-        initialValues={{ username: '', email: '', password: '' }}
-        onSubmit={(values) => handleSignUp(values)}
-        validate={(values) => {
-          const errors = {};
-          if (!values.username) {
-            errors.username = 'Le nom d\'utilisateur est requis';
-          }
-          if (!values.email) {
-            errors.email = 'L\'adresse email est requise';
-          } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(values.email)) {
-            errors.email = 'Adresse email invalide';
-          }
-          if (!values.password) {
-            errors.password = 'Le mot de passe est requis';
-          } else if (values.password.length < 6) {
-            errors.password = 'Le mot de passe doit contenir au moins 6 caractères';
-          }
-          return errors;
-        }}
-      >
-        {({ handleChange, handleBlur, handleSubmit, values, errors }) => (
-          <View>
-            <TextInput
-              style={styles.input}
-              placeholder="Username"
-              onChangeText={handleChange('username')}
-              onBlur={handleBlur('username')}
-              value={values.username}
-            />
-            <Text style={styles.errorText}>{errors.username}</Text>
+                <TextInput
+                  style={styles.input}
+                  placeholder="Email"
+                  onChangeText={handleChange('email')}
+                  onBlur={handleBlur('email')}
+                  value={values.email}
+                />
+                <Text style={styles.errorText}>{errors.email}</Text>
 
-            <TextInput
-              style={styles.input}
-              placeholder="Email"
-              onChangeText={handleChange('email')}
-              onBlur={handleBlur('email')}
-              value={values.email}
-            />
-            <Text style={styles.errorText}>{errors.email}</Text>
+                <TextInput
+                  style={styles.input}
+                  placeholder="Password"
+                  secureTextEntry={true}
+                  onChangeText={handleChange('password')}
+                  onBlur={handleBlur('password')}
+                  value={values.password}
+                />
+                <Text style={styles.errorText}>{errors.password}</Text>
 
-            <TextInput
-              style={styles.input}
-              placeholder="Password"
-              secureTextEntry={true}
-              onChangeText={handleChange('password')}
-              onBlur={handleBlur('password')}
-              value={values.password}
-            />
-            <Text style={styles.errorText}>{errors.password}</Text>
-
-            <Button title="Sign Up" onPress={handleSubmit} />
-          </View>
-        )}
-      </Formik>
-    </KeyboardAvoidingView>
+                <Button title="Sign Up" onPress={handleSubmit} />
+              </View>
+            )}
+          </Formik>
+        </View>
+      </KeyboardAvoidingView>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center",
+    alignItems: 'center',
+  },
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#f9f9f9', // Couleur de fond
+    width: '100%',
+    padding: 20,
   },
   inputContainer: {
-    width: '80%',
-    marginBottom: 20,
+    width: '100%',
+    alignItems: 'center',
   },
   logo: {
     width: 150,
@@ -102,30 +113,16 @@ const styles = StyleSheet.create({
   },
   input: {
     height: 50,
-    backgroundColor: '#fff', 
     borderWidth: 1,
-    borderColor: '#ccc',
+    borderColor: '#009Cff',
     borderRadius: 10,
     paddingHorizontal: 100,
     fontSize: 16,
+    color:'black',
+    fontWeight: 'bold',
   },
   errorText: {
     color: 'red',
-    marginTop: 5,
     fontSize: 12,
   },
-  button: {
-    width: '80%',
-    backgroundColor: '#007bff', 
-    padding: 15,
-    borderRadius: 10,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  buttonText: {
-    color: '#fff', 
-    fontSize: 18,
-    fontWeight: 'bold',
-  },
 });
-
